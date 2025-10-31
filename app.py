d943a400e51a8d685dc315433126aee20a634723
Crimson-elle <aldanaranda.16@gmail.com>
2025-10-31 14:10:04 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+from BD import get_products, insert_sale
+from waitress import serve
+app = Flask(__name__) 
+@app.route('/products', methods=['GET'])
+def products():
+    productos = get_products()
+    return jsonify(productos)
+@app.post('/pedidos')
+def pedidos():
+    if insert_sale(request.json['cliente'], request.json['carrito'], request.json['total']):
+        return jsonify({"status": "success"}), 201
+    else:
+        return jsonify({"status": "error"}), 500
+if __name__ == '__main__':
+    serve(app, host='127.0.0.1', port=5000)
